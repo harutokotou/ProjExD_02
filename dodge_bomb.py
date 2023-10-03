105f5f7bf1537aefd5069e62250985ba0ff843bc
c0b22169 <c0b22169dd@edu.teu.ac.jp>
2023-10-03 15:26:50 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -52,6 +52,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bd_rct): # 練習5：ぶつかってたら
+            print("ゲームオーバー")
+            return
+        
         screen.blit(bg_img, [0, 0])
 
         """こうかとん"""
